6ca97346eff74a4f646fb958fc7adba010e357ce
Diljxt Singh <sdiljot660@gmail.com>
2025-01-30 15:31:15 +0000

    task iii edited

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -5,11 +5,10 @@ ask prompt = do
     if line == "quit"
         then putStrLn "quitting..."
     else if line == ""
-        then ask prompt
+        then ask (prompt ++ "!")  -- Add "!" to the prompt on each empty input
     else do
         putStrLn ("you said: " ++ reverse line)
-        ask prompt
-
+        ask prompt  -- Keep the same prompt for normal input
 
 main = do
     let prompt = "please say something"
